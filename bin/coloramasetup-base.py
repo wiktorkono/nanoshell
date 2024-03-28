a305938bb2fa65bc3253ec5dd5fa55d464ed15ef
Kwadrat Projects <wiktorkono@gmail.com>
2024-03-28 21:08:25 +0100

    update gitignore so bin isn't ignored

---
+++ bin/coloramasetup-base.py
@@ -0,0 +1,11 @@
+from colorama import Fore as c
+from colorama import Back as bg
+from colorama import Style as s
+
+white = c.WHITE
+dim = s.DIM
+bright = s.BRIGHT
+green = c.GREEN
+light_green = c.LIGHTGREEN_EX
+red = c.RED
+r = s.RESET_ALL
