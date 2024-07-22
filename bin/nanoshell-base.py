486b7cbf6123fb76164ebe4e23dba0f08196acb3
jajtic <jajtic.mihael@gmail.com>
2024-07-22 12:59:11 +0200

    Simplify platform check in platformWarning

--- bin/nanoshell-base.py
+++ bin/nanoshell-base.py
@@ -4,16 +4,12 @@ from imports import *
 from bin.coloramasetup import *
 
 running = True
-versionStr = "0.1.3"
+versionStr = "0.1.4"
 
 system_platform = {platform-placeholder} #run.py replaces it with the system platform
 
 def platformWarning():
-    if system_platform == "win":
-        print(f"{red}You're attempting to run a command from an addon that was made specifically for another platform. Due to possible instability, you can't use commands from this addon.{r}")
-    if system_platform == "linux":
-        print(f"{red}You're attempting to run a command from an addon that was made specifically for another platform. Due to possible instability, you can't use commands from this addon.{r}")
-    if system_platform == "darwin":
+    if system_platform == "win" or system_platform == "linux" or system_platform == "darwin":
         print(f"{red}You're attempting to run a command from an addon that was made specifically for another platform. Due to possible instability, you can't use commands from this addon.{r}")
     else:
         print("Nanoshell was unable to identify your system platform. Please use a supported platform (win/linux/darwin).")
