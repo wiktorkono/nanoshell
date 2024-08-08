1877276d181463ee2010d7e07815761351fa5275
Mihael Jajtić <jajtic.mihael@gmail.com>
2024-08-08 13:08:01 +0200

    0.1.5 - move nanoshell variables to nanoshell_lib.py

--- bin/nanoshell-base.py
+++ bin/nanoshell-base.py
@@ -2,19 +2,17 @@ import os
 
 from imports import *
 from bin.coloramasetup import *
+from bin.nanoshell_lib import versionStr, systemPlatform
 
 running = True
-versionStr = "0.1.4"
-
-system_platform = {platform-placeholder} #run.py replaces it with the system platform
 
 def platformWarning():
-    if system_platform == "win" or system_platform == "linux" or system_platform == "darwin":
+    if systemPlatform == "win" or systemPlatform == "linux" or systemPlatform == "darwin":
         print(f"{red}You're attempting to run a command from an addon that was made specifically for another platform. Due to possible instability, you can't use commands from this addon.{r}")
     else:
         print("Nanoshell was unable to identify your system platform. Please use a supported platform (win/linux/darwin).")
 
-print(f"{a}{bright}nanoshell v{versionStr} {a}on {system_platform}{dim}{white} - {r}{white}(C) Hansquare, jajtic 2024")
+print(f"{a}{bright}nanoshell v{versionStr} {a}on {systemPlatform}{dim}{white} - {r}{white}(C) Hansquare, jajtic 2024")
 
 while running:
     prompt = input(f"{bright}{a}nanoshell{dim}{white} > {r}")
---
+++ bin/nanoshell_lib-base.py
@@ -0,0 +1,9 @@
+# Hello there! If you're a developer planning on improving this, or adding more values,
+# consider keeping all variable names camelCase for consistency. Thanks ;)
+
+versionStr = "0.1.5" # NANOSHELL VERSION
+systemPlatform = {system-platform-placeholder}
+
+addonCount = {nanoshell-addonCount-placeholder}
+addonScriptCount = {nanoshell-addonScriptCount-placeholder}
+addonList = {nanoshell-addonList-placeholder}
