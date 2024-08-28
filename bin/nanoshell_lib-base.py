5a9ddf773a265191c64ef0e36617bcd8254c1446
Mihael Jajtić <jajtic.mihael@gmail.com>
2024-08-28 12:03:26 +0200

    Add linuxDistroName to the library (v0.1.7)

--- bin/nanoshell_lib-base.py
+++ bin/nanoshell_lib-base.py
@@ -1,10 +1,11 @@
 # Hello there! If you're a developer planning on improving this, or adding more values,
 # consider keeping all variable names camelCase for consistency. Thanks ;)
 
-versionStr = "0.1.6" # NANOSHELL VERSION
+versionStr = "0.1.7" # NANOSHELL VERSION
 
 addonCount = {nanoshell-addonCount-placeholder}
 addonScriptCount = {nanoshell-addonScriptCount-placeholder}
 addonList = {nanoshell-addonList-placeholder}
 
 systemPlatform = {system-platform-placeholder}
+linuxDistroName = {system-linux-distro-id-placeholder}
